808ed65e77ef1357be520d7e5d413430738d0fd8
chandrabalan001 <chandrabalan001@gmai.com>
2022-11-06 14:52:50 +0530

    added

---
+++ src/test/java/org/com/GITTask.java
@@ -0,0 +1,13 @@
+package org.com;
+
+public class GITTask {
+	public void one() {
+		System.out.println("one");
+}
+	public void two() {
+		System.out.println("two");
+}
+	public void three() {
+		System.out.println("three");
+}
+}
